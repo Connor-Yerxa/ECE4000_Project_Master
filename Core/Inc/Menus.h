7a483321d371f538c411f33c974042019281c9fe
Nathan <r3sse@unb.ca>
2026-03-10 16:19:49 -0300

    version2

--- Core/Inc/Menus.h
+++ Core/Inc/Menus.h
@@ -8,6 +8,21 @@
 #ifndef INC_MENUS_H_
 #define INC_MENUS_H_
 
+#include <stdio.h>
+
+// ---------- Global variables ----------
+extern int screen;           // Current screen
+extern int button;           // Last button pressed
+extern int number;           // Number input from numberSelect
+extern double deltaTemp;     // Current temperature for test
+extern double deltaTime;     // Current time for test
+extern double deltaTempDefault;  // Default temperature
+extern double deltaTimeDefault;  // Default duration
+extern int brightness;       // Display brightness
+extern int both;             // Flag for "both" option in test
+
+// ---------- Functions ----------
+void menus();                // Main menu loop
 
 
 #endif /* INC_MENUS_H_ */
