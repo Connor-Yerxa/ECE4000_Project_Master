83a50120c07b4261375bc0b64140cbace4a82a85
Nathan <r3sse@unb.ca>
2026-03-13 10:11:38 -0300

    calibration v0.5

--- Core/Src/runCalibration.c
+++ Core/Src/runCalibration.c
@@ -7,21 +7,27 @@
 
 #include <stdio.h>
 #include "runTest.h"
+#include "main.h"
 //#include "deleteTest.h"
-//#include <showGraph.h>
+
+double calCoef; // calibration coefficent
 
 int runCalibration(int type){
-	double k;
-	k = runTest(180, 100, 1); //run a test
+	double k2;
+	double k1;
 
-//	deleteTest(); //delete the test
+	k2 = runTest(180, 100, 1); //run a test MIGHT NEED TO CHANGE k2 later, as currently runTest returns 0
+	k2 = calcultateK(); //recalculating k2 as technically runTest doesn't calculate yet.
+	//	deleteTest(); //delete the test
 
 	if (type == 1){
-		//have some alteration to the runTest process
+		k1 = 0.598; // W/m*k WATER at 20 c
 	}
 	else{
 		//have some alteration to the runTest process
+		k1 = 0.285; //GLYCERIN at 26.8 c
 	}
+	calCoef = k1/k2;
 
-	return type;
+	return calCoef;
 }
